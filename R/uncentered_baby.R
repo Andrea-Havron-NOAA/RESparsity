1f3e4b737f6ee273861643145e8554d68f833595
JaneSullivan-NOAA <jane.sullivan@noaa.gov>
2025-07-25 10:17:43 -0800

    update

--- R/uncentered_baby.R
+++ R/uncentered_baby.R
@@ -57,7 +57,7 @@ par$logF <- matrix(0, nrow=length(dat$year), ncol=max(dat$keyF)+1)
 par$missing <- numeric(sum(is.na(dat$logobs)))
 par$tPhi <- if(dat$ar1code==-2){numeric(0)}else{(1)} # AR phi for recruitment
 
-par$z <-  rnorm(length(dat$year), 0, 0.1)#rep(0, length(dat$year))
+par$z <- rep(0, length(dat$year)) #rnorm(length(dat$year), 0, 0.1)
 ##############
 
 itrans <- function(x){
@@ -78,46 +78,48 @@ ssbFUN <- function(logN, logFF, M, SW, MO, PF, PM){
 
 jnll<-function(par){
   getAll(par, dat)
-  
+
   nobs <- length(logobs)
   nrow <- nrow(M)
   ncol <- ncol(M)
-  
+
   sdR <- exp(logsdR)
   sdS <- exp(logsdS)
   sdF <- exp(logsdF)
   sd <- exp(logsd)
-  
+
   phi <- 2*plogis(tPhi)-1
-  
+
   logobs <- logobs+numeric(1) ## hack to make advector
   logobs[is.na(logobs)] <- missing  ##patch missing
-  
+
   logFF <- logF[,keyF+1] ## expand F
-  
-  
+
+
   ssb <- ssbFUN(logN,logFF,M,SW,MO,PF,PM)
-  
+
   jnll <- 0
-  
+
   # non-centered
   logrec <- numeric(nrow)
   logrec[1] <- z[1]*sdR / sqrt(1-phi^2)
   for(y in 2:nrow) {
     logrec[y] <- phi*logrec[y-1] + sdR*z[y]
   }
-  
+
   # logN copy for calcs *** not sure about this part
-  logN_rec <- matrix(0, nrow=nrow, ncol=ncol)
-  logN_rec[,-1] <- logN[,-1]
-  logN_rec[, 1] <- logrec
+  # logN_rec <- matrix(0, nrow=nrow, ncol=ncol)
+  # logN_rec[,-1] <- logN[,-1]
+  # logN_rec[, 1] <- logrec
+
+  logN[, 1] <- logrec
 
   # if(ar1code == -1){
-  
+
   for(y in 2:nrow){
-    
+
     thisSSB <- ifelse((y-minAge-1)>(-.5),ssb[y-minAge],ssb[1])
-    
+
     if(srmode==0){
       predN <- logrec[y-1]
     }
@@ -127,56 +129,56 @@ jnll<-function(par){
     if(srmode==2){
       predN <- bhpar[1]+log(thisSSB)-log(1.0+exp(bhpar[2])*thisSSB)
     }
-    
+
     jnll <- jnll - dnorm(logrec[y],predN,sdR,log=TRUE)
   }
-  
+
   # remaining N matrix
   for(y in 2:nrow){
     for(a in 2:ncol){
-      predN <- logN_rec[y-1,a-1]-exp(logFF[y-1,a-1])-M[y-1,a-1]
+      predN <- logN[y-1,a-1]-exp(logFF[y-1,a-1])-M[y-1,a-1]
       if(a==ncol){
-        predN <- log(exp(predN)+exp(logN_rec[y-1,a]-exp(logFF[y-1,a])-M[y-1,a]))
+        predN <- log(exp(predN)+exp(logN[y-1,a]-exp(logFF[y-1,a])-M[y-1,a]))
       }
-      jnll <- jnll - dnorm(logN_rec[y,a],predN,sdS,log=TRUE)
+      jnll <- jnll - dnorm(logN[y,a],predN,sdS,log=TRUE)
     }
   }
-  
+
   # }
 
   #################### F part - note there are some logN_rec's below
   SigmaF <- matrix(0,ncol(logF),ncol(logF))
-  
+
   if(fcormode==0){
     diag(SigmaF) <- sdF*sdF
   }
-  
+
   if(fcormode==1){
     diag(SigmaF) <- sdF*sdF
     rhoF <- itrans(transRhoF[1])
     for(i in 2:ncol(logF)){
       for(j in 1:(i-1)){
         SigmaF[i,j] <- rhoF*sdF[i]*sdF[j]
-        SigmaF[j,i] <- SigmaF[i,j] 
+        SigmaF[j,i] <- SigmaF[i,j]
       }
     }
   }
-  
+
   if(fcormode==2){
     diag(SigmaF) <- sdF*sdF
     rhoF <- itrans(transRhoF[1])
     for(i in 2:ncol(logF)){
       for(j in 1:(i-1)){
         SigmaF[i,j] <- sdF[i]*sdF[j]*(rhoF^(i-j))
-        SigmaF[j,i] <- SigmaF[i,j] 
+        SigmaF[j,i] <- SigmaF[i,j]
       }
     }
   }
-  
+
   for(y in 2:nrow){
     jnll <- jnll - dmvnorm(logF[y,],logF[y-1,],SigmaF,log=TRUE)
   }
-  
+
   logPred <- numeric(nobs)
   for(i in 1:nobs){
     y <- aux[i,1]-minYear+1
@@ -185,14 +187,14 @@ jnll<-function(par){
     Z <- exp(logFF[y,a])+M[y,a]
     if(fleetTypes[f]==0){
       # Change logN
-      logPred[i] <- logN_rec[y,a]-log(Z)+log(1-exp(-Z))+logFF[y,a]
+      logPred[i] <- logN[y,a]-log(Z)+log(1-exp(-Z))+logFF[y,a]
     }
     if(fleetTypes[f]==2){
-      # Change logN 
-      logPred[i] <- logQ[keyQ[f,a]+1]+logN_rec[y,a]-Z*sampleTimes[f]
+      # Change logN
+      logPred[i] <- logQ[keyQ[f,a]+1]+logN[y,a]-Z*sampleTimes[f]
     }
   }
-  
+
   Svec <- list()
   for(f in 1:nrow(idx1)){
     thisdim <- sum(!is.na(keySd)[f,])
@@ -200,7 +202,7 @@ jnll<-function(par){
     if(covType[f]==0){
       diag(S) <- sd[na.omit(keySd[f,])+1]^2
     }
-    if(covType[f]==1){  
+    if(covType[f]==1){
       dist <- numeric(thisdim)
       d=2;
       for(a in 1:ncol(keyIGAR)){
@@ -232,18 +234,18 @@ jnll<-function(par){
       while(ii<f){from=from+noParUS[ii]; ii<-ii+1}
       thispar <- parUS[from:(from+noParUS[f]-1)]
       U <- diag(thisdim)
-      U[upper.tri(U)] <- thispar   
+      U[upper.tri(U)] <- thispar
       R <- cov2cor(t(U)%*%(U))
       D <- diag(sdvec)
       S <- D%*%R%*%D
     }
     Svec[[f]] <- S;
   }
-  
+
   for(f in 1:nrow(idx1)){
     for(y in 1:ncol(idx1)){
       if(!is.na(idx1[f,y])){
-        idx <- (idx1[f,y]+1):(idx2[f,y]+1)     
+        idx <- (idx1[f,y]+1):(idx2[f,y]+1)
         jnll <- jnll -  dmvnorm(logobs[idx],logPred[idx],Svec[[f]],log=TRUE)
       }
     }
@@ -254,19 +256,19 @@ jnll<-function(par){
   jnll
 }
 
-
+# map for logN (don't estimate rec, instead will be estimated as devs z)
 nyr <- length(dat$year)
 nage <- length(dat$age)
 logN_map <- matrix(1:(nyr * nage), nyr, nage)
 logN_map[,1] <- NA  # fix rec col (est as devs in par$z)
-logN_map <- as.factor(logN_map)  
+logN_map <- as.factor(logN_map)
 
-obj <- MakeADFun(jnll, par, random=c("z", "logN", "logF", "missing"), 
+obj <- MakeADFun(jnll, par, random=c("z", "logN", "logF", "missing"),
                  map=list(logsdF=as.factor(rep(0,length(par$logsdF))),
-                          logN=logN_map), 
+                          logN=logN_map),
                  silent=FALSE)
 
-opt <- nlminb(obj$par, obj$fn, obj$gr, control=list(eval.max=1000, iter.max=1000))
+opt <- nlminb(obj$par, obj$fn, obj$gr) #, control=list(eval.max=1000, iter.max=1000))
 opt$objective
 
 
